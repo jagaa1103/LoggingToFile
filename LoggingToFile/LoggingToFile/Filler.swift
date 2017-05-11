f08a333dc636ce4262e86f83386b8113ff47c1eb
jagaa <jagaa1103@gmail.com>
2017-05-11 17:13:17 +0200

    Create File by Date

--- LoggingToFile/LoggingToFile/Filler.swift
+++ LoggingToFile/LoggingToFile/Filler.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class Filer: NSObject {
+    
+    override init() {
+        super.init()
+    }
+    
+    var file: URL? = nil
+    func createFile(name: String) -> URL? {
+        guard let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else { return nil }
+        guard let writePath = NSURL(fileURLWithPath: path).appendingPathComponent("Logs") else { return nil }
+        try? FileManager.default.createDirectory(atPath: writePath.path, withIntermediateDirectories: true)
+        file = writePath.appendingPathComponent(name + ".txt")
+        return file
+    }
+    
+    func write(text: String) {
+        if let file = checkFile() {
+            try? text.write(to: file, atomically: false, encoding: String.Encoding.utf8)
+        }
+    }
+    
+    func checkFile() -> URL? {
+        if let f = file {
+            return f
+        } else {
+            return createFile(name: getDate())
+        }
+    }
+    
+    func getDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd-hh_mm_ss"
+        return dateFormatter.string(from: Date())
+    }
+}
